ffa36f380ba842374cd3bbf9f2205f418351a5ee
mkaryadi <matthew.t.karyadi@gmail.com>
2023-02-24 14:19:46 -0800

    Re-added firebase starter code

--- capybara/frontend/src/App.js
+++ capybara/frontend/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {getDatabase, ref, get} from 'firebase/database';
 
+const db = getDatabase();
+const sportsRef = ref(db, "category/0/name")
+var sportsTitle;
+get(sportsRef).then((snapshot) => {
+  if (snapshot.exists()) {
+    sportsTitle = snapshot.val();
+  } else {
+    console.log("No data available");
+  }
+})
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- capybara/frontend/src/index.js
+++ capybara/frontend/src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBilDzyLdKGh8zuH-Crnl_4Jyj9QdQZLXs",
+  authDomain: "sharer-21a0a.firebaseapp.com",
+  databaseURL: "https://sharer-21a0a-default-rtdb.firebaseio.com",
+  projectId: "sharer-21a0a",
+  storageBucket: "sharer-21a0a.appspot.com",
+  messagingSenderId: "753912898108",
+  appId: "1:753912898108:web:f324520aa8196b75775939",
+  measurementId: "G-8TBL7QQQXB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
