56b4e28de37deb635fff487bb5f7d0431765a7f3
jwu23-stack <jwu23@uw.edu>
2023-05-05 16:13:32 -0700

    rebased main branch with homepage

--- capybara/frontend/src/widget/Category.js
+++ capybara/frontend/src/widget/Category.js
@@ -5,29 +5,35 @@ import '../stylesheets/App.css';
 
 export function Category(props) {
   const cardStyle = {
-    backgroundImage: `url('https://capybara.s3.us-west-2.amazonaws.com/catalog/Sports.png')`,
+    backgroundImage: `url(${props.image})`,
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
-    minWidth: "280px",
-    minHeight: "320px",
+    minWidth: "260px",
+    minHeight: "300px",
     maxWidth: "300px",
     maxHeight: "340px",
+    borderRadius: "10px",
     display: "flex",
     flexDirection: "column",
     justifyContent: "flex-end",
     alignItems: "flex-start",
+    marginRight: "2em"
   };
 
-  const titleStyle ={
-    marginLeft: "40px",
-    marginBottom: "70px"
+  const titleStyle = {
+    marginLeft: "30px",
+    marginBottom: "40px",
+    maxWidth: "100px",
+    wordWrap: "break-word",
+    color: "#F2F7F5"
   }
 
   return (
-    <div style={cardStyle}>
-      <h2 style={titleStyle}>Sports</h2>
-    </div>
+    <Link className="link-underline link-underline-opacity-0" to={{ pathname: props.goTo, state: { title: props.title }}}>
+      <div style={cardStyle}>
+        <h2 style={titleStyle}>{props.title}</h2>
+      </div>
+    </Link>
   )
-
 }
